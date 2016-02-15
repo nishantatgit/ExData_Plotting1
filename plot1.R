73f513bf9fbe7fdebafafb8ae74b648d0ffccf0e
nishantatgit <nk.muzaffarpur@gmail.com>
2016-02-15 16:32:28 +0530

    first commit

---
+++ plot1.R
@@ -0,0 +1,6 @@
+c_data <- read.csv(file="household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE)
+c_data <- transform(c_data, Date = as.Date(Date,"%d/%m/%Y"))
+f_data <- subset(c_data, Date >= "2007-02-01" & Date <= "2007-02-02")
+f_data <- transform(f_data,Global_active_power = as.numeric(Global_active_power))
+dev.copy(png, file="plot1.png", width = 480 , height = 480 , units = "px")
+dev.off(which = dev.cur())
